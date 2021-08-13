2c6511a4d609385801d592fc672604a1104420ed
Murilo Menezes <snadragona12bionic@gmail.com>
2021-08-13 17:29:40 -0300

    automation script for updating the dotfiles repository

--- update-repo.sh
+++ update-repo.sh
@@ -8,19 +8,41 @@ repeat_str() {
 }
 
 load_status_bar() {
-	local sleep_time=$(perl -E "say(8 / $WINDOW_SIZE);")
+	local sleep_time=$(perl -E "say(${1:-5} / $WINDOW_SIZE);")
 	for i in $(seq $WINDOW_SIZE); do
 		printf "%s%s\r" $'\033[K' "[$(repeat_str "=" $i)>$(repeat_str " " $(( $WINDOW_SIZE - $i )))]"
 		sleep $sleep_time
 	done
+	echo "[$( repeat_str "=" $(( $WINDOW_SIZE + 1 )) )>]"
+	echo Done.
 }
 
 copy_files() {
-	local files_to_copy=("$CH/awesome" "$CH/nvim" "$CH/rofi" "$HOME/.Xresources" $(ls /home/renamed/.zshrc*))
+	local files_to_copy=
+
+	if test -z $1; then
+		files_to_copy=("$CH/awesome" "$CH/nvim" "$CH/rofi" "$HOME/.Xresources" $(ls /home/renamed/.zshrc*))
+	else
+		files_to_copy=($1)
+	fi
 
 	for file in $files_to_copy; do
 		cp -r $file .
 	done
 }
 
-copy_files & load_status_bar
+if [ -z $1 ]; then
+	echo "Copying configuration files." && copy_files & load_status_bar
+else
+	echo "Copying selected configuration files."
+	for arg in $@; do
+		case $arg in
+			neovim|nvim) copy_files "$CH/nvim"&load_status_bar 1;;
+			awesome) copy_files "$CH/awesome"&load_status_bar 1;;
+			rofi) copy_files "$CH/rofi"&load_status_bar 1;;
+			xterm) copy_files "$HOME/.Xresources"&load_status_bar 1;;
+			zsh) for file in $(ls /home/renamed/.zshrc*); do copy_files $file &; done&load_status_bar 1;;
+			*) echo "Unexpected Argument: $arg"
+		esac
+	done
+fi
