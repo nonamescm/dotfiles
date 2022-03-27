9956b38861736303201b56520d6519cba501c63d
nonamescm <snadragona12bionic@gmail.com>
2022-03-27 13:39:05 +0000

    make st bg solid

--- st/config.def.h
+++ st/config.def.h
@@ -94,7 +94,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.93;
+float alpha = 1;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
