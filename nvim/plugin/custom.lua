ba60f4503f83dfdcb49ccba7b1489f2f05d51b24
nonamescm <snadragona12bionic@gmail.com>
2022-03-31 12:03:51 +0000

    syntax error fix!!

--- nvim/plugin/custom.lua
+++ nvim/plugin/custom.lua
@@ -1,3 +1,3 @@
 local hi = require("functions.highlight")
-hi("EndOfBuffer" { guibg = "NONE" })
-hi("Normal" { guibg = "NONE" })
+hi("EndOfBuffer", { guibg = "NONE" })
+hi("Normal", { guibg = "NONE" })
