80594967c7623c9ce627b1f0e45c6aa9382ce90e
nonamescm <snadragona12bionic@gmail.com>
2022-03-31 11:44:26 +0000

    add a script that automatically setups the symlinks

---
+++ setup-ln.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+for dir in */; do
+	echo -n "$dir: install? [y/N] "
+	read answer
+
+	if [ "$answer" != "y" ]; then
+		echo "skipping $dir"
+		continue
+	fi
+
+	if test -d ~/.config/$dir; then
+		echo "~/.config/$dir already exists"
+		echo -n "are you sure you want to overwrite it? [y/N] "
+		read answer
+	fi
+
+	if [ "$answer" != "y" ]; then
+		echo "skipping $dir"
+		continue
+	fi
+
+	dir=$(awk -F/ '{print $1}' <<< $dir)
+	path="$HOME/.config/$dir"
+
+	rm -rf "$path"
+
+	ln -s $( realpath $dir ) ~/.config/
+done
