a2d3bedfce65d842f6034a9f04dd790d5936d53b
nonamescm <snadragona12bionic@gmail.com>
2022-05-05 20:10:12 -0300

    too lazy to ...

--- nvim/plugin/nvimtree.lua
+++ nvim/plugin/nvimtree.lua
@@ -3,10 +3,6 @@ local g = vim.g
 g.nvim_tree_side = "left"
 g.nvim_tree_width = 30
 
-g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_ignore = {".git", "node_modules", ".cache", "target"}
-g.nvim_tree_quit_on_open = 0
-
 g.nvim_tree_git_hl = 0
 g.nvim_tree_root_folder_modifier = ":~"
 g.nvim_tree_allow_resize = 1
