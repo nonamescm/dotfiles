e7358b0839149978b4016759930305db43ae1153
nonamescm <snadragona12bionic@gmail.com>
2023-06-05 15:22:35 -0300

    honestly I forgot what this changes

--- .zshrc
+++ .zshrc
@@ -1,6 +1,8 @@
 source ~/.zshrc-opts
 source ~/.zshrc-theme
 source ~/.zshrc-alias
-[ -f "/home/noname/.ghcup/env" ] && source "/home/noname/.ghcup/env" # ghcup-env
+#[ -f "/home/noname/.ghcup/env" ] && source "/home/noname/.ghcup/env" # ghcup-env
 
 alias luamake=/tmp/lua-language-server/3rd/luamake/luamake
+
+[ -f "/home/noname/.ghcup/env" ] && source "/home/noname/.ghcup/env" # ghcup-env
--- eww/scripts/volume-stats.sh
+++ eww/scripts/volume-stats.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-[ "$(amixer sget 'Master' | grep '\[off\]')" ] && echo "ﱙ" || echo "ﱘ"
+[ "$(amixer sget 'Master' | grep '\[off\]')" ] && echo "󰎊" || echo "󰎈"
 
 pactl subscribe | grep "'change' on sink" --line-buffered | while read; do
-	[ "$(amixer sget 'Master' | grep '\[off\]')" ] && echo "ﱙ" || echo "ﱘ"
+	[ "$(amixer sget 'Master' | grep '\[off\]')" ] && echo "󰎊" || echo "󰎈"
 done
