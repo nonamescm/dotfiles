e1b7883a952e803e2a458ac4200ea13800d35e26
nonamescm <snadragona12bionic@gmail.com>
2026-03-21 02:34:17 -0300

    fix hyprland animations

--- nix/home-manager/modules/hyprland.nix
+++ nix/home-manager/modules/hyprland.nix
@@ -39,13 +39,16 @@
       };
     };
 
-    animations = [
-      "windowsOut, 1, 7, default, popin 80%"
-      "border, 1, 10, default"
-      "borderangle, 1, 8, default"
-      "fade, 1, 7, default"
-      "workspaces, 1, 6, default"
-    ];
+    animations = {
+      enabled = true;
+      animation = [
+        "windowsOut, 1, 7, default, popin 80%"
+        "border, 1, 10, default"
+        "borderangle, 1, 8, default"
+        "fade, 1, 7, default"
+        "workspaces, 1, 6, default"
+      ];
+    };
 
     dwindle = {
       pseudotile = true;
