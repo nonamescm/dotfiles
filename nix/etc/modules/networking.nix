02365692ba7812afc5d57210ea537a576e43b60a
nonamescm <snadragona12bionic@gmail.com>
2026-03-21 00:03:39 -0300

    fix syntax error

--- nix/etc/modules/networking.nix
+++ nix/etc/modules/networking.nix
@@ -1,6 +1,6 @@
-{ config, pkgs, ... }:
+{ ... }:
 {
-  networking.ModemManager.enable = false;
+  networking.modemmanager.enable = false;
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
   time.timeZone = "America/Sao_Paulo";
